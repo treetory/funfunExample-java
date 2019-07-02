eff5dfd3762c77ff3e40eb274549a50da8f8a564
treetory <cokebear@cokebear.local>
2019-07-03 07:29:13 +0900

    틀린 것 수정해가고 있

--- src/main/java/io/funfun/redbook/ConsList.java
+++ src/main/java/io/funfun/redbook/ConsList.java
@@ -25,18 +25,16 @@ public abstract class ConsList<T> {
     }
 
     public <R> ConsList<R> map(ConsList<R> acc, Function<T, R> mapper) {
-        /*
         if (this instanceof Nil) {
             return acc;
         } else {
             return this.tail.map(new Cons<R>(mapper.apply(this.head), acc), mapper);
         }
-        */
-        return isEmpty() ? acc : this.tail.map(new Cons<R>(mapper.apply(this.head), acc), mapper);
+        //return isEmpty() ? acc : this.tail.map(new Cons<R>(mapper.apply(this.head), acc), mapper);
     }
 
     public ConsList<T> filter(ConsList<T> acc, Predicate<T> predicate) {
-        /*
+
         if (this instanceof Nil) {
             return acc;
         } else {
@@ -48,12 +46,19 @@ public abstract class ConsList<T> {
             }
             return this.tail.filter(temp, predicate);
         }
-        */
-        return isEmpty() ? acc : (predicate.test(this.head) ? new Cons<T>(this.head, acc) : this.tail.filter(acc, predicate));
+
+        //return (this instanceof Nil) ? acc : (predicate.test(this.head) ? new Cons<T>(this.head, acc) : this.tail.filter(acc, predicate));
     }
 
     public ConsList<T> flatMap(ConsList<T> acc, Function<ConsList<T>, ConsList<T>> mapper) {
         return isEmpty() ? acc : mapper.apply(this.tail);
     }
 
+    @Override
+    public String toString() {
+        return "ConsList{" +
+                "head=" + head +
+                ", tail=" + tail +
+                '}';
+    }
 }
--- src/main/java/io/funfun/redbook/Main.java
+++ src/main/java/io/funfun/redbook/Main.java
@@ -1,13 +1,34 @@
 package io.funfun.redbook;
 
+import java.util.function.Predicate;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
 
-        System.out.println(FPList.cons(1, FPList.cons(2, FPList.none())));
+        //System.out.println(FPList.cons(1, FPList.cons(2, FPList.none())));
+
+        ConsList consList = new Cons(1, new Cons(2, new Cons(3, new Cons(4, Nil.getNil()))));
+
+        //System.out.println(consList.toString());
+
+        System.out.println(consList.map(Nil.getNil(), o -> (int)o + 1).toString());
+
+        System.out.println(consList.filter(Nil.getNil(), o -> {
+            if ((int)o != 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }).toString());
+
+        System.out.println(consList.flatMap(Nil.getNil(), Main::apply).toString());
 
     }
 
+    private static Object apply(Object consList1) {
+        
+    }
 }
