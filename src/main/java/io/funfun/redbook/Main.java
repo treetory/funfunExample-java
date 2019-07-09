a6ce0d25f556f97e095101d316e1ce1fb2e43355
treetory <cokebear@cokebear.local>
2019-07-09 14:27:08 +0900

    불필요한거 정

--- src/main/java/io/funfun/redbook/Main.java
+++ src/main/java/io/funfun/redbook/Main.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
