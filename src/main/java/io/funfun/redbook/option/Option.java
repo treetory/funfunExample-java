b2575cb3fdabfebbec92dcfec707079f7b93f66a
inhwan.chun <inhwan.chun@lemonhc.com>
2019-07-31 09:45:37 +0900

    Option 은 리뷰결과 반영함 -> 맞는지 한번 더 확인 필요

--- src/main/java/io/funfun/redbook/option/Option.java
+++ src/main/java/io/funfun/redbook/option/Option.java
@@ -13,19 +13,31 @@ public abstract class Option<T> {
         return new Some<>(t);
     }
 
-    // TODO 자기 자신이 Nil 일 때, Nil 리턴하도록 처리할 것
+    // 2019-07-30 리뷰 : 자기 자신이 Nil 일 때, Nil 리턴하도록 처리할 것
     public Option<T> filter(Predicate<T> predicate) {
-        return predicate.test(this.value) ? new Some<>(this.value) : Nil.getNil();
+        if (this instanceof Nil) {
+            return Nil.getNil();
+        } else {
+            return predicate.test(this.value) ? new Some<>(this.value) : Nil.getNil();
+        }
     }
 
-    // TODO 자기 자신이 Nil 일 때, Nil 리턴하도록 처리할 것
+    // 2019-07-30 리뷰 : 자기 자신이 Nil 일 때, Nil 리턴하도록 처리할 것
     public <R> Option<R> map(Function<T, ? extends R> mapper) {
-        return new Some<>(mapper.apply(this.value));
+        if (this instanceof Nil) {
+            return Nil.getNil();
+        } else {
+            return new Some<>(mapper.apply(this.value));
+        }
     }
 
-    // TODO 자기 자신이 Nil 일 때, Nil 리턴하도록 처리할 것
+    // 2019-07-30 리뷰 : 자기 자신이 Nil 일 때, Nil 리턴하도록 처리할 것
     public Option<T> getOrElse() {
-        return (get() instanceof Nil) ? Nil.getNil() : this;
+        if (this instanceof Nil) {
+            return Nil.getNil();
+        } else {
+            return (get() instanceof Nil) ? Nil.getNil() : this;
+        }
     }
 
 }
